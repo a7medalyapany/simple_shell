ff242689b1ebb1f9d6ebf79d97e0a6e034230e1a
a7medalyapany <sci.ahmedibrahim76837@alexu.edu.eg>
2023-08-23 23:45:54 +0300

    initial

--- advanced.c
+++ advanced.c
@@ -10,7 +10,7 @@ int buffAdd(char *buffer, char *str_to_add)
 {
     int length, i;
 
-    length = str_length(buffer);
+    length = myStrLen(buffer);
     for (i = 0; str_to_add[i]; i++)
     {
         buffer[length + i] = str_to_add[i];
@@ -30,25 +30,25 @@ void expVars(progData *data)
     int i, j;
     char line[BUFFER_SIZE] = {0}, expansion[BUFFER_SIZE] = {'\0'}, *temp;
 
-    if (data->input_line == NULL)
+    if (data->inLine == NULL)
         return;
-    buffer_add(line, data->input_line);
+    buffAdd(line, data->inLine);
     for (i = 0; line[i]; i++)
         if (line[i] == '#')
             line[i--] = '\0';
         else if (line[i] == '$' && line[i + 1] == '?')
         {
             line[i] = '\0';
-            long_to_string(errno, expansion, 10);
-            buffer_add(line, expansion);
-            buffer_add(line, data->input_line + i + 2);
+            longStr(errno, expansion, 10);
+            buffAdd(line, expansion);
+            buffAdd(line, data->inLine + i + 2);
         }
         else if (line[i] == '$' && line[i + 1] == '$')
         {
             line[i] = '\0';
-            long_to_string(getpid(), expansion, 10);
-            buffer_add(line, expansion);
-            buffer_add(line, data->input_line + i + 2);
+            longStr(getpid(), expansion, 10);
+            buffAdd(line, expansion);
+            buffAdd(line, data->inLine + i + 2);
         }
         else if (line[i] == '$' && (line[i + 1] == ' ' || line[i + 1] == '\0'))
             continue;
@@ -56,15 +56,15 @@ void expVars(progData *data)
         {
             for (j = 1; line[i + j] && line[i + j] != ' '; j++)
                 expansion[j - 1] = line[i + j];
-            temp = env_get_key(expansion, data);
+            temp = envGK(expansion, data);
             line[i] = '\0', expansion[0] = '\0';
-            buffer_add(expansion, line + i + j);
-            temp ? buffer_add(line, temp) : 1;
-            buffer_add(line, expansion);
+            buffAdd(expansion, line + i + j);
+            temp ? buffAdd(line, temp) : 1;
+            buffAdd(line, expansion);
         }
-    if (!str_compare(data->input_line, line, 0))
+    if (!myStrCmp(data->inLine, line, 0))
     {
-        free(data->input_line);
-        data->input_line = str_duplicate(line);
+        free(data->inLine);
+        data->inLine = myStrDup(line);
     }
 }
--- advanced2.c
+++ advanced2.c
@@ -11,10 +11,10 @@ void expAlias(progData *data)
     int i, j, was_expanded = 0;
     char line[BUFFER_SIZE] = {0}, expansion[BUFFER_SIZE] = {'\0'}, *temp;
 
-    if (data->input_line == NULL)
+    if (data->inLine == NULL)
         return;
 
-    buffer_add(line, data->input_line);
+    buffAdd(line, data->inLine);
 
     for (i = 0; line[i]; i++)
     {
@@ -22,22 +22,22 @@ void expAlias(progData *data)
             expansion[j] = line[i + j];
         expansion[j] = '\0';
 
-        temp = get_alias(data, expansion);
+        temp = getAlias(data, expansion);
         if (temp)
         {
             expansion[0] = '\0';
-            buffer_add(expansion, line + i + j);
+            buffAdd(expansion, line + i + j);
             line[i] = '\0';
-            buffer_add(line, temp);
-            line[str_length(line)] = '\0';
-            buffer_add(line, expansion);
+            buffAdd(line, temp);
+            line[myStrLen(line)] = '\0';
+            buffAdd(line, expansion);
             was_expanded = 1;
         }
         break;
     }
     if (was_expanded)
     {
-        free(data->input_line);
-        data->input_line = str_duplicate(line);
+        free(data->inLine);
+        data->inLine = myStrDup(line);
     }
 }
