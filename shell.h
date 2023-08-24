2813a0e94612529ce29f9b637f606ce1b4e83e4d
a7medalyapany <103336732+a7medalyapany@users.noreply.github.com>
2023-08-24 03:10:26 +0300

    initial

--- shell.h
+++ shell.h
@@ -1,23 +1,19 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-#include <stdio.h>  /* for printf*/
-#include <unistd.h> /* for fork, execve*/
+#include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
-#include <string.h> /* for strtok*/
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <string.h>
 #include <stddef.h>
-#include <errno.h>     /* for errno and perror */
-#include <sys/types.h> /* for type pid */
-#include <sys/wait.h>  /* for wait */
-#include <sys/stat.h>  /* for use of stat function */
-#include <signal.h>    /* for signal management */
-#include <fcntl.h>     /* for open files*/
+#include <errno.h>
+#include <signal.h>
+#include <fcntl.h>
 
-/************* MACROS **************/
-
-#include "macros.h" /* for msg help and prompt */
-
-/************* STRUCTURES **************/
+#include "macros.h"
 
 /**
  * struct info- struct for the program's data
@@ -53,104 +49,63 @@ typedef struct builtins
     int (*function)(progData *data);
 } builtins;
 
-/************* MAIN FUNCTIONS *************/
-
-/*========  shell.c  ========*/
 
 void initData(progData *data, int arc, char *argv[], char **env);
 void showPrompt(char *prompt, progData *data);
 void hCRTLC(int opr UNUSED);
 
-/*========  myGetLine.c  ========*/
-int myGetLine(progData *data);
-int chkOps(char *arrCmd[], int i, char arrOPs[]);
-
-/*======== expansions.c ========*/
-
-/* expand variables */
-void expVars(progData *data);
-void expAlias(progData *data);
-int buffAdd(char *buffer, char *str_to_add);
-
-/*======== str_tok.c ========*/
+int execute(progData *data);
 
 void tokenize(progData *data);
 char *myStrTok(char *line, char *delim);
 
-/*======== execute.c ========*/
+int myGetLine(progData *data);
+int chkOps(char *arrCmd[], int i, char arrOPs[]);
 
-int execute(progData *data);
-
-/*======== buList.c ========*/
-
-int buList(progData *data);
-
-/*======== find_in_path.c ========*/
+void expVars(progData *data);
+void expAlias(progData *data);
+int buffAdd(char *buffer, char *str_to_add);
 
 char **tokenizePath(progData *data);
 int findProg(progData *data);
 
-/************** HELPERS FOR MEMORY MANAGEMENT **************/
+int buList(progData *data);
 
-/*======== helpers_free.c ========*/
-
-void freeArr(char **directories);
-void freeCD(progData *data);
-void freeAD(progData *data);
-
-/************** BUILTINS **************/
-
-/*======== builtins_more.c ========*/
-
-int myExit(progData *data);
-int myCD(progData *data);
-int setWD(progData *data, char *newDir);
-int myHelp(progData *data);
-int myAlias(progData *data);
-
-/*======== builtins_env.c ========*/
+int myStrLen(char *string);
+char *myStrCon(char *string1, char *string2);
+char *myStrDup(char *string);
+int myStrCmp(char *string1, char *string2, int number);
+void myStrRev(char *string);
 
 int myEnv(progData *data);
 int mySetEnv(progData *data);
 int myUnSetEnv(progData *data);
 
-/************** HELPERS FOR ENVIRONMENT VARIABLES MANAGEMENT **************/
-
-/*======== env_management.c ========*/
-
-char *envGK(char *name, progData *data);
-int envSK(char *key, char *value, progData *data);
-int envRK(char *key, progData *data);
-void printEnv(progData *data);
-
-/************** HELPERS FOR PRINTING **************/
-
-/*======== helpersaiPrint.c ========*/
-
-int aiPrint(char *string);
 int aiPrinte(char *string);
 int aiPrintErr(int errorcode, progData *data);
+int aiPrint(char *string);
 
-/************** HELPERS FOR STRINGS MANAGEMENT **************/
+int myCD(progData *data);
+int setWD(progData *data, char *newDir);
+int myAlias(progData *data);
+int myHelp(progData *data);
+int myExit(progData *data);
 
-/*======== helpers_string.c ========*/
+void freeArr(char **directories);
+void freeCD(progData *data);
+void freeAD(progData *data);
 
-int myStrLen(char *string);
-char *myStrDup(char *string);
-int myStrCmp(char *string1, char *string2, int number);
-char *myStrCon(char *string1, char *string2);
-void myStrRev(char *string);
-
-/*======== helpers_numbers.c ========*/
+void printEnv(progData *data);
+int envRK(char *key, progData *data);
+char *envGK(char *name, progData *data);
+int envSK(char *key, char *value, progData *data);
 
 void longStr(long number, char *string, int base);
 int myAtoi(char *s);
 int countChars(char *string, char *character);
 
-/*======== alias_management.c ========*/
-
-int printAlias(progData *data, char *alias);
 char *getAlias(progData *data, char *alias);
 int setAlias(char *alias_string, progData *data);
+int printAlias(progData *data, char *alias);
 
 #endif
